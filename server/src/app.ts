2139983856bbff0924536f7577df2f08606eb7ca
Atul <atulmalviyav2@gmail.com>
2024-08-03 02:27:25 +0530

    Mongo connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import {connectDB} from "./DB/dbConn";
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
