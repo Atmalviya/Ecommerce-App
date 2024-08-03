23fac8366ef34940ef745de07215d1d2ec1c82bd
Atul <atulmalviyav2@gmail.com>
2024-08-03 16:17:04 +0530

    protected route implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,50 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import ProductListPage from './pages/ProductListPage';
 import CartPage from './pages/CartPage';
 import AdminPage from './pages/AdminPage';
 import SignupPage from './pages/SignupPage';
+import { toast } from 'sonner';
+
+const AuthRoute = ({ children }: { children: React.ReactNode }) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return <Navigate to="/products" />;
+  }
+  return <>{children}</>;
+};
+
+const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    // If no token is found, user is not authenticated
+    toast.error("Please login first");
+    return <Navigate to="/login" />;
+  }
+  return <>{children}</>; // If authenticated, render the child components
+};
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path="/products" element={<ProductListPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<AuthRoute>
+              <LoginPage />
+            </AuthRoute>} />
+        <Route path='/signup' element={<AuthRoute>
+              <SignupPage />
+            </AuthRoute>} />
+        <Route path="/products" element={<PrivateRoute>
+              <ProductListPage />
+            </PrivateRoute>} />
+        <Route path="/cart" element={<PrivateRoute>
+              <CartPage />
+            </PrivateRoute>} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/" element={<LoginPage />} />
+        <Route path="*" element={<LoginPage />} />
       </Routes>
     </Router>
   );
