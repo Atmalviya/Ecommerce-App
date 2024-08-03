10fb3328cbf1c206ad2c41ae4305e145a7471fbf
Atul <atulmalviyav2@gmail.com>
2024-08-03 16:35:54 +0530

    existing user check during new user registration

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -7,8 +7,10 @@ export const register = async (req: Request, res: Response) => {
   const { email, password, role } = req.body;
 
   try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ error: 'User already exists' });
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ email, password: hashedPassword, role });
+    const user = new User({ email, password: hashedPassword, role : role || 'user' });
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully' });
@@ -19,20 +21,19 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-
   try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ error: 'User not found' });
-
-    const isMatch = await bcrypt.compare(password, user.password);
+      const user = await User.findOne({ email });
+      if (!user) return res.status(404).json({ error: 'User not found' });
+      
+      const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
 
     const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET!, {
-      expiresIn: '1h'
+      expiresIn: '1d'
     });
 
     res.json({ token, message: `Hello, ${email}!` });
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'Server error', error });
   }
 };
