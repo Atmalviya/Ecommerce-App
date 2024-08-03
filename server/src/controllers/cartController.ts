d41599949103448d7ebed4b80fd22d4c6379bfeb
Atul <atulmalviyav2@gmail.com>
2024-08-03 13:18:06 +0530

    cart checkout api implement

--- server/src/controllers/cartController.ts
+++ server/src/controllers/cartController.ts
@@ -38,3 +38,23 @@ export const viewCart = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
+
+export const checkoutCart = async (req: Request, res: Response) => {
+  const { shippingAddress } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ user: req.user.userId });
+    if (!cart) return res.status(404).json({ error: 'Cart not found' });
+
+    cart.shippingAddress = shippingAddress;
+    await cart.save();
+
+    cart.products = [];
+    await cart.save();
+
+
+    res.json({ message: 'Checkout successful' });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { addToCart, viewCart } from '../controllers/cartController';
+import { addToCart, checkoutCart, viewCart } from '../controllers/cartController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/add', authMiddleware, addToCart);
 router.get('/view', authMiddleware, viewCart);
+router.post('/checkout', authMiddleware, checkoutCart);
 
 export default router;
