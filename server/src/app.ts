52979a415f9f0cd559991dad99b9e0855f9b0d67
Atul <atulmalviyav2@gmail.com>
2024-08-04 17:10:49 +0530

    CORS error resolve

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import {connectDB} from "./DB/dbConn";
-import authRoutes from "./routes/authRoutes";
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './DB/dbConn';
+import authRoutes from './routes/authRoutes';
 import cartRoutes from './routes/cartRoutes';
 import productRoutes from './routes/productRoutes';
 
@@ -10,18 +10,27 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions: cors.CorsOptions = {
+  origin: 'http://localhost:5173/', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
+
 connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.use('/uploads', express.static('uploads'));
-app.use("/api/auth", authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/products', productRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
