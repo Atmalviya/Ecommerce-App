ee89b9a48f2e8047f6cd2fd976369a0d6e9cc403
Atul <atulmalviyav2@gmail.com>
2024-08-04 12:58:10 +0530

    Route protection and toast notifiaction added

--- client2/src/App.jsx
+++ client2/src/App.jsx
@@ -11,16 +11,66 @@ import LoginPage from "./pages/LoginPage";
 import ProductListPage from "./pages/ProductListPage";
 import CartPage from "./pages/CartPage";
 import AdminPage from "./pages/AdminPage";
+const token = localStorage.getItem("token");
+
+const AuthRoute = ({ children }) => {
+  if (token) {
+    return <Navigate to="/products" />;
+  }
+  return <>{children}</>;
+};
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/products" element={<ProductListPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route
+          path="/login"
+          element={
+            <AuthRoute>
+              <LoginPage />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthRoute>
+              <SignupPage />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <PrivateRoute>
+              <ProductListPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <CartPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminPage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="*" element={<LoginPage />} />
       </Routes>
     </Router>
   );
